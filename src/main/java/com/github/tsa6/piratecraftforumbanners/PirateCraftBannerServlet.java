130077918f21b0d6f6f84507586e7fb93e78b71e
Taizo Simpson <tsa6games@gmail.com>
2016-10-06 18:51:19 -0400

    Fixed user agent to allow program to decide.

--- src/main/java/com/github/tsa6/piratecraftforumbanners/PirateCraftBannerServlet.java
+++ src/main/java/com/github/tsa6/piratecraftforumbanners/PirateCraftBannerServlet.java
@@ -2,26 +2,20 @@ package com.github.tsa6.piratecraftforumbanners;
 
 import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 public class PirateCraftBannerServlet extends BannerServlet {
 
-	private static final URL url;
+	private static final URL URL;
 	static {
 		try {
-			url = new URL("http://stats.piratemc.com/");
+			URL = new URL("http://stats.piratemc.com/");
 		} catch (MalformedURLException ex) {
 			throw new Error(ex);
 		}
 	}
 
-	public PirateCraftBannerServlet() {//Fix
-		super(url);
-		if(System.getProperty("http.agent") == null) {
-			System.setProperty("http.agent", "StatForumBanners/1.0(Contact tsa6games@gmail.com)");
-			Logger.getLogger(this.getClass().getCanonicalName()).log(Level.INFO, "Updated http.agent to {0}", System.getProperty("http.agent"));
-		}
+	public PirateCraftBannerServlet() {
+		super(URL);
 	}
 
 }
